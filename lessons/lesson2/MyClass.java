ef52cb4909a345011aeac08a59e7f5644c50f8ec
Dayro Richard Rios Piñan <145415131+Addicted2you@users.noreply.github.com>
2025-11-11 21:06:29 -0500

    Add MyClass.java with primitive and non-primitive types

---
+++ lessons/lesson2/MyClass.java
@@ -0,0 +1,26 @@
+public class MyClass {
+    
+    
+  public static void main(String args[]) {
+    
+    //tipos de datos primitivos
+    int entero=1;
+    float f= 1.3f;
+    double d= 3.5;
+    char letra= 'a';
+    boolean bool = true;
+    
+    //tipos de datos no primitivos
+    int[] arrayEnteros={1,2,3};
+    String cadena = "lección 2";
+    
+    System.out.println(entero);
+    System.out.println(f);
+    System.out.println(d);
+    System.out.println(letra);
+    System.out.println(bool);
+    System.out.println(arrayEnteros[0] + " " + arrayEnteros[1] + " " +arrayEnteros[2]);
+    System.out.println(cadena);
+    
+  }
+}
